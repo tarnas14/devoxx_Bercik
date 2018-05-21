a33ac6d6e8a9ae030b812e5ac413477b79d1657d
tarnas14 <tarnas14@gmail.com>
2018-05-21 21:46:47 +0200

    added twitter library and a test endpoint getting tweets of authenticated user

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const authentication = require('./authentication')
 const logger = require('morgan')
+const Twitter = require('twitter')
 
 const app = express()
 
@@ -17,6 +18,21 @@ authentication.setup(app)
 authentication.routes(app)
 
 app.use('/contestant', express.static(path.join(__dirname, 'contestant_app')))
+
+app.get('/api/tweets/', (req, res) => {
+  const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: req.user.creds.token,
+    access_token_secret: req.user.creds.tokenSecret,
+  })
+
+  client.get('statuses/home_timeline', (error, tweets, response) => {
+    if(error) throw error;
+    console.log('[tweets]', tweets, response)
+    res.json(tweets)
+  });
+})
 
 const {PORT} = process.env
 app.listen(PORT, () => {
